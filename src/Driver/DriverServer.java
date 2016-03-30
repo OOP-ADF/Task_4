108ec35db6c05362080bb3b51c0d30bde8d20146
agungrbudiman <agungrbudiman@gmail.com>
2016-03-30 20:25:24 +0700

    sampai DriverServer.java

---
+++ src/Driver/DriverServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+import javaChat.ServerConnection;
+
+/**
+ *
+ * @author agungrb
+ */
+public class DriverServer {
+    public static void main(String[] args) {
+        try {
+            ServerConnection server = new ServerConnection();
+            System.out.println("Server Information");
+            System.out.println(server.getServerInformation());
+        } catch (Exception e) {
+        }
+    }
+}
---
+++ src/chatGUI/ChatController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatGUI;
+import java.awt.event.ActionEvent;
+import javaChat.ClientConnection;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author agungrb
+ */
+public class ChatController implements ActionListener {
+    private ChatView view;
+    private ClientConnection client = null;
+    
+    public ChatController() {
+        view = new ChatView();
+        view.setVisible(true);
+        view.addListener(this);
+        client = null;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+        try {
+            String ip = view.getStringChat();
+            client.connect(ip);
+            
+            WriteOutput w = new WriteOutput();
+            w.start();
+        } catch (Exception e) {
+            System.out.println("Ada error atau masalah teknis");
+        }
+        String input = view.getStringChat();
+        client.writeStream(input);
+        view.setTxFieldChat("");
+    }
+    
+    class WriteOutput extends Thread {
+        public void run() {
+            try {
+                String inputan;
+                while ((inputan = client.readStream()) != null) {
+                    view.setTxAreaChat(inputan);
+                }
+            } catch (Exception e) {
+                System.out.println("Ada error atau masalah teknis");
+            }
+        }
+    }
+}
