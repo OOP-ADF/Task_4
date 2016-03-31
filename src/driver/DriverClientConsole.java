a7bd82671a210fde29374b359f3787e47dcfddb2
yulizar <Erwin@10.5.15.23>
2016-03-31 20:17:38 +0700

    Merge origin/master
    Erwin Yulizar F || 1301144031

---
+++ src/driver/DriverClientConsole.java
@@ -0,0 +1,14 @@
+package driver;
+
+import consoleApp.ConsoleApplication;
+
+/**
+ *
+ * @author Erwin
+ */
+public class DriverClientConsole {
+    public static void main(String[] args) {
+        ConsoleApplication console = new ConsoleApplication();
+        console.startChat();
+    }
+}
