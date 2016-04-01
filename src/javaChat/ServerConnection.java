7b348230c6acc6b1c639b8ba647090f8d7df5771
Adam Geraldy <adam.geraldy@gmail.com>
2016-04-01 18:58:20 +0700

    First

--- src/javaChat/ServerConnection.java
+++ src/javaChat/ServerConnection.java
@@ -1,9 +1,10 @@
+package javaChat;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaChat;
+
 
 import java.io.IOException;
 import java.net.InetAddress;
