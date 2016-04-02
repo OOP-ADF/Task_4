7f00a742fafceff8825cc6501193543cfa3a53eb
M. Yuslan Abubakar <M. Yuslan Abubakar@DESKTOP-IJ3QVCA>
2016-04-02 21:47:46 +0700

    Done

---
+++ src/chatGUI/ChatController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javaChat.ClientConnection;
+
+/**
+ *
+ * @author M. Yuslan Abubakar
+ */
+public class ChatController implements ActionListener {
+    private ChatView view;
+    private ClientConnection client = null;
+    
+    public ChatController() {
+        view = new ChatView();
+        view.setVisible(true);
+        view.addListener(this);
+        client = null;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+        
+        if (source == view.getTxFieldChat()) {
+            if (client == null) {
+                try {
+                    ClientConnection client = new ClientConnection();
+                    String ip = view.getStringChat();
+                    client.connect(ip);
+                    
+                    WriteOutput w = new WriteOutput();
+                    w.start();
+                } catch (Exception e) {
+                    System.out.println("Error : " + e.getMessage());
+                }
+            }
+            else {
+                String input = view.getStringChat();
+                client.writeStream(input);
+            }
+        }
+        view.setTxFieldChat("");
+    }
+    
+    class WriteOutput extends Thread {
+        public void run() {
+            try { 
+                String inputan;
+                while((inputan = client.readStream()) != null) {
+                    view.setTxAreaChat(inputan);
+                }
+            } catch (Exception e) {
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+    }
+}
