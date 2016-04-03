14d5a16fc1f7af48c6c8dcf79fba5d8a27a0d4a5
Metteora <Metteora@Metteora-PC>
2016-04-04 00:03:34 +0700

    Edited & Done by Reynaldi Ananda Pane IF-38-09 (1301144099)

---
+++ src/Driver/DriverClientGUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+import chatGUI.ChatController;
+
+/**
+ *
+ * @author Reynaldi Ananda Pane
+ * 1301144099
+ * IF-38-09
+ */
+public class DriverClientGUI {
+    public static void main(String[] args) {
+        ChatController cc = new ChatController();
+    }
+}
