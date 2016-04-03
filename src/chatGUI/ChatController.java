7ba72e51b0f67d6a77561a6ca7fa0c975c4e13fd
rizzauliarahman <rizzauliarahman@gmail.com>
2016-04-03 23:22:15 +0700

    Modify Task 4 (Add ChatController.java, ChatView.java, connectionThread.java, consoleApplication.java, DriverClientConsole.java, DriverClientGUI.java, and DriverServer.java)
    Name : Rizza Aulia Rahman
    Class : IF-38-01
    Student ID : 1301144231

---
+++ src/chatGUI/ChatController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatGUI;
+
+import java.awt.event.ActionEvent;
+import javaChat.ClientConnection;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Rizza
+ */
+public class ChatController implements ActionListener {
+    ChatView view;
+    ClientConnection client = null;
+
+    public ChatController() {
+        view = new ChatView();
+        view.setVisible(true);
+        view.addListener(this);
+        client = null;
+    }
+    
+    public class writeOutput extends Thread {
+        @Override
+        public void run() {
+            try {
+                String inputan;
+                while ((inputan = client.readStream()) != null) {
+                    view.setTxAreaChat(inputan);
+                }
+            } catch (Exception e) {
+                System.out.println("Error "+ e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+        if (source == view.getTxFieldChat()) {
+            if (client == null) {
+                try {
+                    client = new ClientConnection();
+                    String ip = view.getStringChat();
+                    client.connect(ip);
+                    writeOutput w = new writeOutput();
+                    w.start();
+                } catch (Exception e) {
+                    System.out.println("Error "+ e.getMessage());
+                }
+            } else {
+                String input = view.getStringChat();
+                client.writeStream(input);
+            }
+            view.setTxFieldChat("");
+        }
+    }
+    
+}
---
+++ src/driver/DriverClientGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import chatGUI.*;
+import consoleApp.*;
+import javaChat.*;
+import javaChat.*;
+
+/**
+ *
+ * @author Rizza
+ */
+public class DriverClientGUI {
+    public static void main(String[] args) {
+        ChatController chat = new ChatController();
+    }    
+}
