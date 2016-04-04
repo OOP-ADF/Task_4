72900b2e79d4953e4e42503d336967f987f74411
harizmhd <harizmhd@gmail.com>
2016-04-05 00:19:25 +0700

    Finished Task_4

---
+++ src/driver/DriverClientGui.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import chatGUI.ChatController;
+
+/**
+ * @author Muhammad Hariz Arasy
+ * 1301140259
+ * IF-38-09
+ */
+public class DriverClientGui {
+    public static void main(String[] args) {
+        ChatController cc = new ChatController();
+    }
+}
---
+++ src/driver/DriverServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import consoleApp.ConnectionThread;
+import javaChat.ServerConnection;
+
+/**
+ * @author Muhammad Hariz Arasy
+ * 1301140259
+ * IF-38-09
+ */
+public class DriverServer {
+    public static void main(String[] args) {
+       try {
+           ServerConnection server = new ServerConnection();
+           System.out.println("Server Information");
+           System.out.println(server.getServerInformation());
+           while (true) {
+               ConnectionThread connection
+                       = new ConnectionThread(server.getClient());
+               connection.start();
+           }
+       } catch(Exception e) {
+           System.out.println("Exception occurred: " + e.getMessage());
+       }
+    }
+}
