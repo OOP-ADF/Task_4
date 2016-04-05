a0800f841dbfce25849b4b616d20927f51f01600
anitauliani <anitauliani@yahoo.co.id>
2016-04-05 16:02:10 +0700

    task4

---
+++ src/Driver/DriverClientConsole.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+/**
+ *
+ * @author Anita Auliani IF-38-09 1301144349
+ */
+public class DriverClientConsole {
+    public static void main(String[] args) {
+        ConsoleApplication CApp = new ConsoleApplication();
+         CApp.startChat();
+    }
+    
+}
+
---
+++ src/Driver/DriverClientGui.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+/**
+ *
+ * @author Anita Auliani IF-38-09 1301144349
+ */
+public class DriverClientGui {
+    public static void main(String[] args) {
+        ChatController CC = new ChatController();
+    }
+    
+}
+
