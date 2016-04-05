7857345ea59bffa4f7b5330a371bcc615e92b676
Kurniawan <Kurniawan@Kurniawan>
2016-04-05 22:40:33 +0700

    updated

---
+++ src/driver/DriverServer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import consoleApp.ConnectionThread;
+import javaChat.ServerConnection;
+
+/**
+ *
+ * @author Kurniawan
+ */
+public class DriverServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerConnection server = new ServerConnection();
+            System.out.println("Server Information");
+            System.out.println(server.getServerInformation());
+            while (true) {
+                ConnectionThread connection = new ConnectionThread(server.getClient());
+                connection.start();
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+}
