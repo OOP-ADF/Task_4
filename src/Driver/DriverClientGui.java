6c0fbf993803a73bee8ed079d8167d3600265d90
user <user@10.1.251.119>
2016-04-06 17:18:45 +0700

    Alhamdulillah pak....

---
+++ src/Driver/DriverClientGui.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+import chatGui.ChatController;
+/**
+ *
+ * @author user
+ */
+public class DriverClientGui {
+   public static void main(String[] args){
+        ChatController chatController=new ChatController();
+    }
+}
