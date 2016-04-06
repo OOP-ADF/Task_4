68b5caf3ce4d8c3a33f3f45ed47aacd8a0bfeac6
Adi Nugroho <>
2016-04-06 21:07:24 +0700

    Adi Nugroho - 1301144271

---
+++ src/chatGUI/ChatController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatGUI;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javaChat.ClientConnection;
+public class ChatController implements ActionListener{
+    private ChatView view;
+    private ClientConnection client=null;
+    
+    
+    public ChatController(){
+        view = new ChatView();
+        view.setVisible(true);
+        view.addListener(this);
+        client = null;
+    }
+   class WriteOutput{
+       public void run(){
+           try{
+               String inputan;
+               while ((inputan = client.readStream()) != null){
+                   view.setTxAreaChat(inputan);
+               }
+           }
+           catch(IOException e){
+               System.out.println("Error");
+           }
+       }
+   }
+   public void actionPerformed(ActionEvent ae){
+       Object source = ae.getSource();
+       try{
+           String ip = view.getStringChat();
+           client.connect(ip);
+           WriteOutput w = new WriteOutput();
+           w.run();
+       }
+       catch(IOException e){
+           System.out.println("Error");
+       }
+   }
+   
+}
