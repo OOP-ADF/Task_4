333b9dadd065e2ec47b44f1f02b74e6a84e9e46f
anisaifah <anisaifah@gmail.com>
2016-04-12 02:48:03 +0700

    Added files via upload

---
+++ src/ConsoleApp/ConsoleApplication.java
@@ -0,0 +1,57 @@
+package ConsoleApp;
+
+import javaChat.ClientConnection;
+
+
+/**
+ *
+ * @author Fah
+ */
+public class ConsoleApplication {
+    private ClientConnection client;
+    public void startChat(){
+        try{
+            client = new ClientConnection();
+            System.out.println("inpt server IP : ");
+            String ip = client.inputString();
+            client.connect(ip);
+            ReadInput in = new ReadInput();
+            WriteOutput out = new WriteOutput();
+            in.start();
+            out.start();
+        }catch(Exception e){
+            System.out.println("Error - "+e);
+        }
+    }
+
+    public class ReadInput extends Thread{
+        @Override
+        public void run(){
+            try{
+                String inputKeyboard;
+                do{
+                    System.out.println(">> ");
+                    inputKeyboard = client.inputString();
+                    client.writeStream(inputKeyboard);
+                } while(!inputKeyboard.equals("quit"));
+                client.disconnect();
+            }catch(Exception e){
+                System.out.println("Error -"+e);
+            }
+        }
+    }
+    public class WriteOutput extends Thread{
+        @Override
+        public void run(){
+            try{
+                String inputan;
+                while((inputan = client.readStream()) != null){
+                    System.out.println(inputan);
+                    System.out.println(">> ");
+                }
+            }catch(Exception e){
+                System.out.println("Error -"+e);
+            }
+        }
+    }
+}
---
+++ src/Driver/DriverClientConsole.java
@@ -0,0 +1,15 @@
+package Driver;
+
+import ConsoleApp.ConsoleApplication;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class DriverClientConsole {
+    public static void main(String[] args) {
+        ConsoleApplication ca = new ConsoleApplication();
+        ca.startChat();
+    }
+}
