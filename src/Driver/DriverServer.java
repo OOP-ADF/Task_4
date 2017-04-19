ba0b5b539b1529036fd394e3ca47320563fddfaf
nafasamuth <nafasa.muth@outlook.com>
2017-04-19 18:31:08 +0800

    Nafasa

---
+++ src/Driver/DriverServer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+
+import consoleApp.ConnectionThread;
+import java.io.IOException;
+import javaChat.ServerConnection;
+
+public class DriverServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerConnection serverc = new ServerConnection();
+            System.out.println("Server Information");
+            System.out.println(serverc.getServerInformation());
+            while (true) {
+                ConnectionThread connectionT = new ConnectionThread(serverc.getClient());
+                connectionT.run();
+
+            }
+        } catch (IOException E) {
+            System.out.println("Error");
+        }
+    }
+
+}
---
+++ src/chatGUI/JChatController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javaChat.ClientConnection;
+import javax.swing.text.View;
+
+/**
+ *
+ * @author Farid Kiftirul Aziz
+ */
+public class JChatController implements ActionListener {
+
+    private ChatView view;
+    private ClientConnection client;
+
+    public JChatController() {
+        view = new ChatView();
+        view.setVisible(true);
+        view.addListener(this);
+        client = null;
+    }
+
+    public class WriteOutput extends Thread {
+
+        @Override
+        public void run() {
+            String inputan;
+            try {
+                while ((inputan = client.readStream()) != null) {
+                    view.setTxAreaChat(inputan);
+                }
+            } catch (IOException E) {
+                System.out.println("Error");
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+
+        if (source.equals(view.getTxFieldChat())) {
+            if (client == null) {
+                try {
+                    client = new ClientConnection();
+                    String ip = view.getStringChat();
+                    client.connect(ip);
+                    WriteOutput w = new WriteOutput();
+                    w.start();
+                } catch (IOException E) {
+                    System.out.println("Error");
+                }
+            } else {
+                String input = view.getStringChat();
+                client.writeStream(input);
+                view.setTxFieldChat("");
+            }
+        }
+    }
+}
