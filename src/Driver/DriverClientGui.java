216d29acb5d2b8c3d9a0ffa6c7b8122017d87947
ranestari <ranestari10@gmail.com>
2017-04-20 10:08:14 +0700

    Implementasi

---
+++ src/Driver/DriverClientGui.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+import chatGUI.jChatController;
+import consoleApp.ConnectionThread;
+import javaChat.ServerConnection;
+
+/**
+ *
+ * @author Ranestari Sastriani
+ */
+public class DriverClientGui {
+    
+   public static void main(String[] args) {
+        jChatController Jchat = new jChatController();
+    }
+ 
+}
