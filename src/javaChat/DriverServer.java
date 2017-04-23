84cc999335c9ad511dbadf6d2f5cd28e6d13e8eb
serliirc <serliiratmalacrusita@gmail.com>
2017-04-23 15:47:42 +0700

    1301154474_Serlii_IF 39-12
    Driver

---
+++ src/javaChat/DriverServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaChat;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Serlii R. Crusita
+ */
+public class DriverServer {
+    
+  public static void main(String[] args) throws IOException {
+        try {
+            ServerConnection server = new ServerConnection();
+            System.out.println("Server Information");
+            System.out.println(server.getServerInformation());
+            while (true){
+                ConnectionThread connection = new ConnectionThread(server.getClient());
+                connection.run();
+            }
+        }
+        catch(IOException E){
+            System.out.println("Error");
+        }
+    }
+    
+}
