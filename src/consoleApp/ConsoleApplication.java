bddba5241859713c24ca77958c3351afa9fe4a76
Kukuh Rahingga P <Kukuh Rahingga P@GxcCyro.mshome.net>
2017-04-27 12:05:41 +0700

    1301150035_Kukuh Rahingga Permadi_IF-39-07_Done

---
+++ src/consoleApp/ConsoleApplication.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consoleApp;
+
+import javaChat.ClientConnection;
+
+/**
+ *
+ * @author Kukuh Rahingga P
+ */
+public class ConsoleApplication {
+    private ClientConnection client;
+    
+    public void startChat() {
+        try {
+            client = new ClientConnection();
+            System.out.println("input server IP : ");
+            String ip = client.inputString();
+            client.connect(ip);
+        
+            ReadInput in = new ReadInput();
+            WriteOutput out = new WriteOutput();
+            in.start();
+            out.start();
+            
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+        
+        
+    }
+    
+    public class ReadInput extends Thread{
+        
+        @Override
+        public void run() {
+            try {
+                String inputKeyboard;
+                do {
+                    System.out.println(">> ");
+                    inputKeyboard = client.inputString();
+                    client.writeStream(inputKeyboard);
+                } while (!inputKeyboard.equals("quit"));
+                client.disconnect();
+                
+            } catch (Exception e) {
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+    }
+    
+    public class WriteOutput extends Thread{
+        
+        @Override
+        public void run() {
+            try {
+                String input;
+                while ((input = client.readStream()) != null) {
+                    System.out.println(input);
+                    System.out.println(">> ");
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/driver/DriverClientGui.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+import chatGUI.ChatController;
+
+/**
+ *
+ * @author Kukuh Rahingga P
+ */
+public class DriverClientGui {
+    public static void main(String[] args) {
+        ChatController chatgui = new ChatController();
+    }
+}
