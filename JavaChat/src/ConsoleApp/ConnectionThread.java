6b1088abe2267d0c47c5e970bee7c4b685e2bc0b
Nindia Cahyaning Putri <already.nindia@yahoo.com>
2017-04-30 21:08:09 +0700

    gui dan console
    gui dan console

--- JavaChat/src/ConsoleApp/ConnectionThread.java
+++ JavaChat/src/ConsoleApp/ConnectionThread.java
@@ -5,6 +5,10 @@
  */
 package ConsoleApp;
 
+import java.io.IOException;
+import java.net.Socket;
+import javachat.Connection;
+
 /**
  *
  * @author Nindia Cahyaning Putri
@@ -12,5 +16,43 @@ package ConsoleApp;
  * IF3907
  */
 public class ConnectionThread {
+    private Socket client;
+    private Connection connection;
     
+    public ConnectionThread(Socket newClient) throws IOException {
+        this.client = newClient;
+        connection = new Connection(client);
+    }
+    
+    
+    
+    public void run() throws IOException{
+        //method yang dijalankan saat thread diaktifkan
+        //Thread akan aktif setiap ada Client baru yang terhubung ke Server
+        //Thread akan berhenti ketika Client mengirimkan pesan "quit"
+        try{
+            connection.startChat("start the chat");
+            System.out.println("----------------------");
+            System.out.println("new client connected");
+            System.out.println("client information: ");
+            System.out.println(connection.getClientInformation());
+            
+            String inputan;
+            String message;
+            while ((inputan = connection.readStream()) !=null && !inputan.equals("quit")){
+                message = "Client " +connection.getIpClient()
+                        + "said : " +inputan;
+                System.out.println(message);
+                connection.sendToAll(message);
+            }
+            message = "Client from IP : " +connection.getIpClient()
+                    +"Quit the char room";
+            System.out.println(message);
+            connection.sendToAll(message);
+            connection.disconnect();
+        }
+        catch (IOException E) {
+            System.out.println("error");
+        }
+    }
 }
--- JavaChat/src/ConsoleApp/ConsoleApplication.java
+++ JavaChat/src/ConsoleApp/ConsoleApplication.java
@@ -5,6 +5,10 @@
  */
 package ConsoleApp;
 
+import java.io.IOException;
+import javachat.ClientConnection;
+import javachat.Connection;
+
 /**
  *
  * @author Nindia Cahyaning Putri
@@ -12,5 +16,51 @@ package ConsoleApp;
  * IF3907
  */
 public class ConsoleApplication {
+    private ClientConnection client;
+    public class ReadInput extends Thread{
+        @Override
+        public void run(){
+            try{
+                String inputKeyboard;
+                do{
+                    System.out.println(">>");
+                    inputKeyboard = client.inputString();
+                    client.writeStream(inputKeyboard);
+                } while (!inputKeyboard.equals("quit"));
+                client.disconnect();
+                
+            }catch (IOExpection E){
+                System.out.println("error");
+                }
+            }
+        }
     
+    public class WriteOutput extends Thread{
+        @Override
+        public void run(){
+            try{
+                String inputan;
+                while ((inputan = client.readStream()) !=null){
+                    System.out.println(inputan);
+                    System.out.println(">>");
+                }
+            } catch (IOException E){
+                System.out.println("error");
+            }
+        }
+    }
+    public void StarChat(){
+        try{
+            client = new Connection();
+            System.out.println("Input server IP: ");
+            String ip = client.inputString();
+            client.connect(ip);
+            ReadInput in = new ReadInput();
+            WriteOutput out = new WriteOutput();
+            in.start();
+            out.start();
+        }catch(IOException E){
+            System.out.println("error");
+        }
+    }
 }
