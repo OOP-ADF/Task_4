d9a6dc4870d49f820520bc75b55d0d813448a87b
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2017-05-06 02:50:21 +0700

    Add files

---
+++ src/consoleApp/ConnectionThread.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package consoleApp;
+
+/**
+ * File Description
+ *
+ * @author Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
+ * @since May 6, 2017
+ */
+public class ConnectionThread {
+
+}
---
+++ src/consoleApp/ConsoleApplication.java
@@ -0,0 +1,17 @@
+package consoleApp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * File Description
+ *
+ * @author Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
+ * @since May 6, 2017
+ */
+public class ConsoleApplication {
+
+}
