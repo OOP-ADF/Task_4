0fe0e1fe20fcdfe9be776b8c2a42c0ae462edae7
Christianjanifer <christiansimanjuntak12@gmail.com>
2017-05-07 18:03:27 +0700

    Task 4 Done Maaf pak terlambat Terima Kasih

---
+++ src/driver/DriveClientConsole.java
@@ -0,0 +1,16 @@
+package driver;
+
+import consoleApp.ConsoleApplication;
+
+/**
+ *
+ * @author Christian Janifer
+ */
+public class DriveClientConsole {
+    
+    public static void main(String[] args) {
+        
+        ConsoleApplication ca = new ConsoleApplication(); 
+        ca.startChat(); 
+    }
+}
