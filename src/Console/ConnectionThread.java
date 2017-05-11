fb1d949db75564f0b76ca6ef83f310d96cad6e35
Alwifaisal <faisalzulfikar05@gmail.com>
2017-05-11 04:06:12 -0700

    1301150007_Alwi faisal

---
+++ src/Console/ConnectionThread.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Console;
+import java.io.IOException;
+import java.net.Socket;
+import javaChat.Connection;
+import javaChat.Connection;
+
+/**
+ *
+ * @author Alwi faisal
+ */
+public class ConnectionThread {
+    private Socket client;
+    private Connection connection;
+
+    public ConnectionThread(Socket newClient) throws IOException {
+        this.client = newClient;
+        connection = new Connection(client);
+    }
+
+
+    
+    public void run() {
+        try {
+            connection.startChat("Start chat");
+            System.out.println("-----------------------");
+            System.out.println("new client connected");
+            System.out.println("client information");
+           System.out.println(connection.getClientInformation());
+            
+            String inputan;
+            String message;
+            while ((inputan = connection.readStream()) != null && !inputan.equals("quit")) {             
+                message = "Client" + connection.getIpClient() + " said : " + inputan;
+                System.out.println(message);
+                connection.sendToAll(message);
+            }
+            
+            message = "Client from IP : " + connection.getIpClient() + "Quit the chat room";
+            System.out.println(message);
+            connection.sendToAll(message);
+           connection.disconnect();
+            
+        } catch (Exception e) {
+           System.out.println("Error : " + e.getMessage());
+        }
+    }
+}
+    
+
