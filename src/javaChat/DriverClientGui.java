0645dbed98f659d3cb62edd758916c7fecc8c915
nikoonovian <niko.novian@yahoo.co.id>
2017-05-16 14:14:24 +0700

    task 4

---
+++ src/javaChat/DriverClientGui.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaChat;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DriverClientGui {
+    public static void main(String[] args) {
+        JChatController jcc = new JChatController(); 
+    }
+}
+
