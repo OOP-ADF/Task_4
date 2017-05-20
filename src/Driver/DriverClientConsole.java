4a135ed2ac633aaac4b6da65d9d7a9c88a9d4230
ergandhiyudha <erganzkiddharmonic@gmail.com>
2017-05-20 23:50:18 +0700

    1301154147_ErgandhiYudhaKurniawan
    TASK 4 siap.

---
+++ src/Driver/DriverClientConsole.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver;
+
+import ConsoleAPP.ConsoleApplication;
+
+/**
+ *
+ * @author Ergandhi Yudha Kurniawan
+ */
+
+public class DriverClientConsole {
+
+    public static void main(String[] args) {
+        ConsoleApplication ca = new ConsoleApplication();
+        ca.startChat();
+    }
+}
