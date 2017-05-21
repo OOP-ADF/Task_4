7d25b6ab5cee5300b015c4b75034641aca1470d2
MRPanda <harddisk1945@gmail.com>
2017-05-21 16:39:35 +0700

    PBO_1301154404_Task4_ADF

---
+++ src/ChatGUI/ChatController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChatGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javaChat.ClientConnection;
+
+/**
+ *
+ * @author Asus
+ */
+public class ChatController implements ActionListener{
+    private ChatView view;
+    private ClientConnection client = null;
+    
+    public ChatController(){
+        view = new ChatView();
+        view.setVisible(true);
+        view.addActionListener(this);
+        client = null;
+    }
+    
+    public class WriteOutput extends Thread{
+        @Override
+        public void run(){
+            try{
+                String inputan;
+                while((inputan = client.readStream()) != null){
+                    view.setTxAreaChat(inputan);
+                }
+                
+            }catch(Exception e){
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object source = ae.getSource();
+        if(source == view.getTxFieldChat()){
+            if(client == null){
+                try{
+                  String ip = view.getStringChat();
+                  client.connect(ip);
+                  
+                  WriteOutput w = new WriteOutput();
+                  w.start();     
+                }catch(Exception e){
+                    System.out.println("Error : " + e.getMessage());
+                }
+            }
+            else{
+                String input = view.getStringChat();
+                client.writeStream(input);
+            }
+            view.setTxField("");
+        }
+    }
+    
+}
