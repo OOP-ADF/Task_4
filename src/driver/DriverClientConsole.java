f8c98da22a758a06d0f56525e32d97c89c590f54
cleanrun <marchellehcram@gmail.com>
2017-05-21 15:54:40 +0700

    Cobain

---
+++ src/driver/DriverClientConsole.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+/**
+ *
+ * @author ichank
+ */
+import consoleApp.ConsoleApplication;
+
+public class DriverClientConsole {
+    public static void main(String[] args) {
+        ConsoleApplication cpp = new ConsoleApplication();
+        cpp.startChat();
+    }
+    
+}
---
+++ src/driver/DriverServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driver;
+
+/**
+ *
+ * @author ichank
+ */
+import consoleApp.connectionthread;
+import javaChat.ServerConnection;
+
+public class DriverServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerConnection server = new ServerConnection();
+            System.out.println("Server Information");
+            System.out.println(server.getServerInformation());
+            while(true){
+                connectionthread connection = new connectionthread(server.getClient());
+                connection.start();
+            }
+        } catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+}
